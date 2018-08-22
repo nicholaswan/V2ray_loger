bd58f0f3ef1e56032a7798dbeb6a507dda5038bd
sjz123321 <shijiezuan@gmail.com>
2018-08-22 09:51:25 +0800

    Signed-off-by: sjz123321 <shijiezuan@gmail.com>

--- install.sh
+++ install.sh
@@ -1,20 +1,16 @@
+#!/bin/bash
+
+chmod 777 *
 echo "运行该脚本前请确定mailx已成功部署 回车键继续"
 read a
 echo "mailx 部署脚本可以使用我的 mailx部署脚本"
 echo "https://github.com/sjz123321/mailx-bash-shell-1  回车键继续"
 read a
 apt-get update && apt-get install gcc -y
-yum update && yum -y install gcc
-c_version=`gcc --version`
-
-if [ "$c_version" != "" ] ; then
-	echo "gcc 安装成功"
-else
-	echo "gcc 安装失败，请检查或手动安装gcc"
-
-mv api.sh /usr/bin/vloger.sh
+apt-get install curl -y
+mv api.sh /usr/v2ray_loger/
 gcc process.c
-mv a.out /usr/bin/process.out
+mv a.out /usr/v2ray_loger/
 cd /tmp
 rm -rf *
 echo "请输入mail地址"
@@ -22,6 +18,6 @@ read temp
 echo $temp >> /etc/v2ray/mail_addr.config
 echo "邮箱地址设置成功"
 echo "将发送一个测试邮件到您设置的邮箱"
-echo "Project V loger installed successfully" | maill -s "Test mail" -a /tmp/test_text.log $temp
+echo "Project V loger installed successfully" | mail -s "Test mail" -a /tmp/test_text.log $temp
 echo "如果您没有收到邮件请检查 邮箱地址是否错误 您可以通过修改/etc/v2ray/mail_addr.config 来修改您设置的邮箱地址"
 
