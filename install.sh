e8b8980208f60d1d6492b91fb428efad2480935b
sjz123321 <37840247+sjz123321@users.noreply.github.com>
2018-08-22 09:58:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ echo "https://github.com/sjz123321/mailx-bash-shell-1  回车键继续"
 read a
 apt-get update && apt-get install gcc -y
 apt-get install curl -y
+mkdir -p /usr/v2ray_loger/
 mv api.sh /usr/v2ray_loger/
 gcc process.c
 mv a.out /usr/v2ray_loger/
@@ -18,6 +19,7 @@ read temp
 echo $temp >> /etc/v2ray/mail_addr.config
 echo "邮箱地址设置成功"
 echo "将发送一个测试邮件到您设置的邮箱"
+echo "test_text" >> /tmp/test_text.log
 echo "Project V loger installed successfully" | mail -s "Test mail" -a /tmp/test_text.log $temp
 echo "如果您没有收到邮件请检查 邮箱地址是否错误 您可以通过修改/etc/v2ray/mail_addr.config 来修改您设置的邮箱地址"
 
